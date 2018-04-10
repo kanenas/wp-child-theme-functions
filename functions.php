c2b720718409832ff13da26b4ed042d19cd37255
Nikolas Branis <nbran@kanenas.net>
2018-04-11 00:38:10 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,10 @@
 <?php
+// Load styles from parent template
+function knn_enqueue_parent_theme_style() {
+	wp_enqueue_style( 'knn-parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'knn_enqueue_parent_theme_style', 99 );
+
 /* Adding DNS Prefetching */
 function knns_dns_prefetch() {
 	echo '<meta http-equiv="x-dns-prefetch-control" content="on">
